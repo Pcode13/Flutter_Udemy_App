499e21fa39226dccc8bbbd8e3ad49232d12e1f8a
Pragati Veer <92431808+Pcode13@users.noreply.github.com>
2025-03-24 15:29:59 +0530

    Flutter Layouts Challenge

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,46 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(
-MaterialApp(
-home: Scaffold(
-  backgroundColor: Colors.red.shade300,
-  appBar: AppBar(
-    title: Text("Flutter App "),
-    backgroundColor: Colors.pinkAccent,
-  ),
-  body:Center(
+  runApp(MyApp());
+}
 
-      child: Image(image:AssetImage("images/dimond.png"))
-  )
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            backgroundColor: Colors.teal,
+            body:SafeArea(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Container(
+                      width: 100.0,
+                      color: Colors.pink,
+                    ),
+                    // SizedBox(width: 30),
+                    Container(
+                     child: Column(
+                       mainAxisAlignment: MainAxisAlignment.center,
+                       children: <Widget>[
+                         Container(width: 100.0,height: 100.0,color: Colors.yellow),
+                      Container(width: 100.0,height: 100.0,color: Colors.yellow.shade300),]
+                     ),
+                    ),
+                    // SizedBox(width: 30),
+                    Container(
+                      width: 100.0,
+                      color: Colors.white70,
+                    ),
+
+                  ],
+                )
+            )
 
 
-)
-)
-);
+        )
+    );
+  }
 }
