e2fe89a9dfa687070d615faaed391247aa2abd12
Pragati Veer <92431808+Pcode13@users.noreply.github.com>
2025-03-24 18:14:03 +0530

    MiCard Completed Project

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
             body:SafeArea(
                 child:
              Column(
-               // mainAxisAlignment: MainAxisAlignment.center,
+               mainAxisAlignment: MainAxisAlignment.center,
                children: [
                  CircleAvatar(
                    radius: 50.0,
@@ -36,7 +36,42 @@ class MyApp extends StatelessWidget {
                      fontWeight:FontWeight.bold,
                      fontFamily: 'SourceSansPro',
                    letterSpacing: 2.5
-                 ),)
+                 ),),
+                 SizedBox(height: 20.0,
+                 width: 150.0,
+                 child: Divider(
+                   color: Colors.teal.shade100,
+                 ),
+                 ),
+                 Card(
+
+                   margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
+                   child: ListTile(
+                     leading: Icon(Icons.phone,color: Colors.teal,),
+                     title:
+                  Text('+91 9999999999',
+                  style:
+                TextStyle(fontSize: 20.0,color: Colors.teal.shade900,
+                   ),
+                     ),
+
+                   )
+                 ),
+                 Card(
+                   margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
+                   child:
+                   ListTile(
+                     leading: Icon(Icons.email,color: Colors.teal,),
+                     title:
+                     Text('veer99@gmail.com',
+                       style:
+                       TextStyle(fontSize: 20.0,color: Colors.teal.shade900,
+                       ),
+                     ),
+
+                   )
+
+                 )
                ],
              )
             )
@@ -46,3 +81,14 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+
+// Row(
+// children: <Widget>[
+// Icon(Icons.email,color: Colors.teal,),
+// SizedBox(width: 10.0,),
+// Text('veer99@gmail.com',style: TextStyle(fontSize: 20.0,color: Colors.teal.shade900,
+// fontFamily: 'SourceSansPro'),
+// )
+// ],
+// )
