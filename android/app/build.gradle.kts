f24bb0ce95affc73f6af0ae89637bb2eb3e723fb
Pragati Veer <92431808+Pcode13@users.noreply.github.com>
2025-03-25 18:51:38 +0530

    Xylophone App - It is Music App

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.testapp.test_app2"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
