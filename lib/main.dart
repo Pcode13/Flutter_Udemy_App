c1192ab56db575136dc8e4a880abe2c42322c87e
Pragati Veer <92431808+Pcode13@users.noreply.github.com>
2025-03-27 12:06:31 +0530

    Creating a Question Class and chcekanswer method update the function

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,7 @@ import 'package:audioplayers/audioplayers.dart';
 
 import 'Screens/QuizApp/question.dart';
 
-
-void main()=>runApp(const QuizzApp());
-
+void main() => runApp(const QuizzApp());
 
 class QuizzApp extends StatelessWidget {
   const QuizzApp({super.key});
@@ -17,16 +15,15 @@ class QuizzApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.grey.shade900,
         body: SafeArea(
-        child:Padding(
-          padding: EdgeInsets.symmetric(horizontal: 10.0),
-          child: Quiz(),
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: Quiz(),
+          ),
         ),
       ),
-    ),
     );
   }
 }
-
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
@@ -36,20 +33,20 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-  List<Icon> scoreKeeper=[];
-  // List<String> question=[
-  //   'You can lead a cow down stairs but not up stairs.',
-  //   'Approximately one quarter of human bones are in the feet.',
-  //   'A slug\'s blood is green.',
-  // ];
-  // Question q1 =Question(questionText:'You can lead a cow down stairs but not up stairs.',questionAnswer:false);
-List<Question> questionList=[
-  Question(questionText: 'You can lead a cow down stairs but not up stairs.', questionAnswer: false),
-  Question(questionText: 'Approximately one quarter of human bones are in the feet.', questionAnswer: true),
-  Question(questionText: 'A slug\'s blood is green.', questionAnswer: true),
-];
-   int questionNumber=0;
-  // List<bool> answers =[false,true,true];
+  List<Icon> scoreKeeper = [];
+
+  List<Question> questionList = [
+    Question(
+      questionText: 'You can lead a cow down stairs but not up stairs.',
+      questionAnswer: false,
+    ),
+    Question(
+      questionText: 'Approximately one quarter of human bones are in the feet.',
+      questionAnswer: true,
+    ),
+    Question(questionText: 'A slug\'s blood is green.', questionAnswer: true),
+  ];
+  int questionNumber = 0;
 
   @override
   void initState() {
@@ -58,91 +55,96 @@ List<Question> questionList=[
     print(questionList[questionNumber].questionAnswer);
   }
 
+  void checkAnswer(bool userAnswer) {
+    bool correctAnswer = questionList[questionNumber].questionAnswer;
+
+    setState(() {
+      if (userAnswer == correctAnswer) {
+        print("User got it right");
+        scoreKeeper.add(const Icon(Icons.check, color: Colors.green));
+      } else {
+        scoreKeeper.add(const Icon(Icons.close, color: Colors.red));
+      }
+      if (questionNumber < questionList.length - 1) {
+        questionNumber++;
+      } else {
+        questionNumber = 0;
+        scoreKeeper = [];
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: <Widget>[
-       Expanded(
-         flex:5,
-           child:Padding(
-             padding:EdgeInsets.all(10.0),
-             child: Center(
-               child: Text(questionList[questionNumber].questionText,
-                 textAlign: TextAlign.center,
-                 style: TextStyle(
-                   fontSize: 25.0,
-                   color: Colors.white,
-                 ),
-               ),
-             ),
-           )
-       ),
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                questionList[questionNumber].questionText,
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 25.0, color: Colors.white),
+              ),
+            ),
+          ),
+        ),
         Expanded(
           child: Padding(
             padding: EdgeInsets.all(5.0),
             child: TextButton(
-              style:ButtonStyle(
+              style: ButtonStyle(
                 backgroundColor: WidgetStateProperty.all<Color>(Colors.green),
               ),
               onPressed: () {
-                bool correctAnswer =questionList[questionNumber].questionAnswer;
-                print('questionNumber true: $correctAnswer');
-                setState(() {
-                  print('questionNumber: $questionNumber');
-                  if(correctAnswer==true){
-                    print("User got it right");
-                    scoreKeeper.add(const Icon(Icons.check, color: Colors.green));
-                  }else{
-                    print("User got it wrong");
-                    scoreKeeper.add(const Icon(Icons.close, color: Colors.green));
-                  }
-                  questionNumber=questionNumber+1;
-                  // scoreKeeper.add(const Icon(Icons.check, color: Colors.green));
-                });
+                checkAnswer(true);
               },
-              child: Text('True',style: TextStyle(fontSize: 20.0,color: Colors.white),),
-            )
+              child: Text(
+                'True',
+                style: TextStyle(fontSize: 20.0, color: Colors.white),
+              ),
+            ),
           ),
         ),
         Expanded(
           child: Padding(
-              padding: EdgeInsets.all(5.0),
-              child: TextButton(
-                style:ButtonStyle(
-                  backgroundColor: WidgetStateProperty.all<Color>(Colors.red),
-                ),
-                onPressed: () {
-                  bool correctAnswer =questionList[questionNumber].questionAnswer;
-print('questionNumber: $correctAnswer');
-                  setState(() {
-                    if(correctAnswer==false){
-                      print("User got it right");
-                      scoreKeeper.add(const Icon(Icons.check, color: Colors.green));
-
-                    }else{
-                      print("User got it wrong");
-                      scoreKeeper.add(const Icon(Icons.close, color: Colors.green));
-                    }
-                  });
-                },
-                child: Text('False',style: TextStyle(fontSize: 20.0,color: Colors.white),),
-              )
+            padding: EdgeInsets.all(5.0),
+            child: TextButton(
+              style: ButtonStyle(
+                backgroundColor: WidgetStateProperty.all<Color>(Colors.red),
+              ),
+              onPressed: () {
+                checkAnswer(false);
+              },
+              child: Text(
+                'False',
+                style: TextStyle(fontSize: 20.0, color: Colors.white),
+              ),
+            ),
           ),
         ),
         //TODO: Add a Row here as your score keeper
-        Row(
-          children: scoreKeeper,
-        )
+        Row(children: scoreKeeper),
       ],
     );
   }
 }
 
-
-
-
-
-
-
+// bool correctAnswer =questionList[questionNumber].questionAnswer;
+// print('questionNumber true: $correctAnswer');
+// setState(() {
+// print('questionNumber: $questionNumber');
+// if(correctAnswer==true){
+// print("User got it right");
+// scoreKeeper.add(const Icon(Icons.check, color: Colors.green));
+// }else{
+// print("User got it wrong");
+// scoreKeeper.add(const Icon(Icons.close, color: Colors.green));
+// }
+// questionNumber=questionNumber+1;
+// // scoreKeeper.add(const Icon(Icons.check, color: Colors.green));
+// });
